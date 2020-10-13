5ebb94bc40fc4f3d0b5ebd402a50d6cc9e462f59
Artur <54704039+Artur-I@users.noreply.github.com>
2020-10-13 23:21:16 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function toReadable (number) {
+  let ones = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+  let tens = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety']
   
+  if (number === 0) return 'zero'
+  if (number < 20) return ones[number]
+
+  let digit = number % 10
+  if (number < 100) {
+    return tens[Math.floor(number / 10)] + (digit ? ' ' + ones[digit] : '')
+  }
+  if (number < 1000) {
+    return ones[Math.floor(number / 100)] + ' hundred' + (number % 100 === 0 ? '' : ' ' + toReadable(number % 100))
+  }
+  //return toReadable(Math.floor(number % 1000)) + ' thousand' + (number % 1000 !== 0 ? ' ' + toReadable(number % 1000) : '')
 }
